e7933b516086858377d7dd276b19a4d7b3429dc9
Christian Castro <fernandochris1995@gmail.com>
2024-10-24 17:32:40 -0500

    Create Script an database

---
+++ DB/SP/GetMenus.sql
@@ -0,0 +1,13 @@
+CREATE OR ALTER PROCEDURE GetMenus
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT DISTINCT m.Id, m.MenuName, m.ParentId, m.Url, m.Icon, m.SortOrder
+    FROM [User] u
+		INNER JOIN UserRole ur ON u.Id = ur.UserId
+		INNER JOIN RoleMenu rm ON ur.RoleId = rm.RoleId
+		INNER JOIN [Menu] m ON rm.MenuId = m.Id
+    WHERE m.IsActive = 'A'
+    ORDER BY m.SortOrder;
+END;
